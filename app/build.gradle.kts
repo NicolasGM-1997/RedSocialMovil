94e7c42486d86991f95c53ae3d31ae71d99e975e
NicolasGomez <nicolas.gomezm@uniagustiniana.edu.co>
2023-11-14 13:38:55 -0500

    Segunda Vista

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.redsocial"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.redsocial"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
